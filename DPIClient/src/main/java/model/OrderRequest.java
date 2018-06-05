a9c4725159311b5881c93b0746899271680ffeea
Luud van Keulen <luudvkeulen@gmail.com>
2018-06-05 20:46:05 +0200

    Added id to orderRequest

--- DPIClient/src/main/java/model/OrderRequest.java
+++ DPIClient/src/main/java/model/OrderRequest.java
@@ -1,9 +1,11 @@
 package model;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class OrderRequest {
 
+    public String id;
     public String type;
     public String subType;
     public Date time;
@@ -13,6 +15,7 @@ public class OrderRequest {
     }
 
     public OrderRequest(String type, String subType) {
+        this.id = UUID.randomUUID().toString();
         this.type = type;
         this.subType = subType;
         this.time = new Date();
