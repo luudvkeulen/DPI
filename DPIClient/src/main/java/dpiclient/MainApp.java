3d623773a678ef481684e378950c3eaebd23fb1a
Luud van Keulen <luudvkeulen@gmail.com>
2018-06-05 18:50:01 +0200

    Init

---
+++ DPIClient/src/main/java/dpiclient/MainApp.java
@@ -0,0 +1,40 @@
+package dpiclient;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import jms.OrderReplyListener;
+
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        
+        stage.setTitle("DPI Client");
+        stage.setScene(scene);
+        stage.show();
+        
+        new OrderReplyListener().listen();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ DPIClient/src/main/java/jms/OrderReplyListener.java
@@ -0,0 +1,37 @@
+package jms;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class OrderReplyListener {
+
+    private static final String QUEUE_NAME = "order_replies";
+
+    public void listen() {
+        try {
+            Gateway gateway = new Gateway();
+            gateway.channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+
+            System.out.println("Listening for order replies...");
+
+            Consumer consumer = new DefaultConsumer(gateway.channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    String message = new String(body);
+                    System.out.println("Received order reply: " + message);
+                }
+            };
+            
+            gateway.channel.basicConsume(QUEUE_NAME, true, consumer);
+        } catch (IOException | TimeoutException ex) {
+            Logger.getLogger(OrderReplyListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
